11b7a076d245dd78a8c41bd4ac7dd2d67ec343ce
julkarzunayed <zunayedjulkar@gmail.com>
2025-06-24 05:43:45 +0600

    added footer and navbar

--- src/layouts/RootLayout/RootLayout.jsx
+++ src/layouts/RootLayout/RootLayout.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Outlet } from 'react-router';
+import NavBar from '../../pages/shared/NavBar/NavBar';
+import Footer from '../../pages/shared/Footer/Footer';
 
 const RootLayout = () => {
     return (
         <div>
-            
+            <NavBar></NavBar>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
